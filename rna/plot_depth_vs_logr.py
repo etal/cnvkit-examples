70a79456f44313061ab329b2b3a06c17bbbc987c
Eric Talevich <eric.talevich@gmail.com>
2018-08-17 21:10:33 -0700

    rna: Generate 'nolimit' set w/o +3 cap on log2 values
    Plot depth vs. log2 as hexbin instead of scatter

--- rna/plot_depth_vs_logr.py
+++ rna/plot_depth_vs_logr.py
@@ -6,9 +6,11 @@ depths (i.e. low gene expression) than when read depths are deeper (higher
 expression).
 
 Columns used:
+
 - depth -- The observed average read depth across the transcribed portion of the
   gene: 'expected_count' from the original RSEM or gene-level read counts (TCGA)
   input file, times read length (100bp), divided by transcript length.
+
 - log2 -- The final bin/gene-level log2 ratio estimates (before smoothing or
   segmentation): depths normalized by sample and gene, log2-scaled; missing/zero
   values replaced by within-gene minima; with GC and transcript-length bias
@@ -21,34 +23,56 @@ import os
 import sys
 
 import cnvlib
-from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
+import seaborn
 
 AP = argparse.ArgumentParser(description=__doc__)
 AP.add_argument('cnr_files', nargs='+', help="All sample .cnr files.")
 AP.add_argument('-o', '--output', help="Output filename.")
 args = AP.parse_args()
 
-acc = []
-logs = []
-depths = []
-for fname in args.cnr_files:
-    cnr = cnvlib.read(fname)
-    # XXX current .cnr files in out/ are clipped at +3
-    logs.append(cnr['log2'])
-    depths.append(cnr['depth'])
-    if len(args.cnr_files) == 1:
-        plt.title(cnr.sample_id)
+seaborn.plotting_context("poster")
+seaborn.set(font="Sans", style="darkgrid")
 
-logs = np.concatenate(logs)
-depths = np.concatenate(depths)
-plt.scatter(depths, logs, marker='o', alpha=.2, edgecolors='none', color='k')
+
+
+def load_depths_logs(cnr_fnames):
+    logs = []
+    depths = []
+    for fname in cnr_fnames:
+        cnr = cnvlib.read(fname)
+        logs.append(cnr['log2'])
+        depths.append(cnr['depth'])
+        # Ninja move
+        if len(cnr_fnames) == 1:
+            plt.title(cnr.sample_id)
+
+    depths = np.concatenate(depths)
+    keep_idx = (depths != 0)
+    depths = depths[keep_idx]
+    logs = np.concatenate(logs)[keep_idx]
+    return depths, logs
+
+
+depths, logs = load_depths_logs(args.cnr_files)
+
 max_x = depths.max() + 1
 plt.hlines(0, 0, max_x)
 plt.xlim(1, max_x)
-plt.xscale('log')
+#plt.xscale('log')
 plt.xlabel('Observed read depth')
 plt.ylabel('Normalized log2 ratio')
+
+
+# TODO use seaborn
+
+#plt.scatter(depths, logs, marker='o', alpha=.2, edgecolors='none', color='k')
+plt.hexbin(depths, logs, gridsize=200, bins='log', mincnt=1,
+           xscale='log',
+           edgecolors='none')
+
+
 if args.output:
     oformat = os.path.splitext(args.output)[-1].replace(".", "")
     plt.savefig(args.output, format=oformat, bbox_inches="tight")
